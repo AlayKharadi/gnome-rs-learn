0194d28ecf4d260a6b1b350ba6ebc237fdc90d13
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2023-06-19 16:06:45 +0530

    subclass: functionality

--- g_object_subclassing/src/custom_button/imp.rs
+++ g_object_subclassing/src/custom_button/imp.rs
@@ -1,25 +1,43 @@
+use std::cell::Cell;
+
 use gtk::{
+    prelude::ButtonExt,
     glib,
     subclass::{
-        prelude::{ButtonImpl, ObjectImpl, ObjectSubclass},
+        prelude::{ButtonImpl, ObjectImpl, ObjectSubclass, ObjectSubclassExt, ObjectImplExt},
         widget::WidgetImpl,
     },
 };
 
+// Object holding the state
 #[derive(Default)]
-pub struct CustomButton;
+pub struct CustomButton {
+    number: Cell<i32>,
+}
 
+// The central trait for subclassing a GObject
 #[glib::object_subclass]
 impl ObjectSubclass for CustomButton {
     const NAME: &'static str = "MyGtkAppCustomButton";
-
     type Type = super::CustomButton;
-
     type ParentType = gtk::Button;
 }
 
-impl ObjectImpl for CustomButton {}
+// Trait shared by all GObjects
+impl ObjectImpl for CustomButton {
+    fn constructed(&self) {
+        self.parent_constructed();
+        self.obj().set_label(&self.number.get().to_string());
+    }
+}
 
+// Trait shared by all widgets
 impl WidgetImpl for CustomButton {}
 
-impl ButtonImpl for CustomButton {}
+// Trait shared by all buttons
+impl ButtonImpl for CustomButton {
+    fn clicked(&self) {
+        self.number.set(self.number.get() + 1);
+        self.obj().set_label(&self.number.get().to_string())
+    }
+}
